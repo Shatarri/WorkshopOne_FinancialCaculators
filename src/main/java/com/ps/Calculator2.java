80751d8a81c19db8fc5e8e9790c1bbffefc4a484
Diamond Dale <dale.diamond@aol.com>
2024-09-29 21:40:14 -0500

    //calculator 2 //getting user info

--- src/main/java/com/ps/Calculator2.java
+++ src/main/java/com/ps/Calculator2.java
@@ -1,7 +1,16 @@
 package com.ps;
+import java.util.Scanner;
 
 public class Calculator2 {
     public static void main(String[] args){
+        Scanner scanner = new scanner(System.in);
+
+        // getting user information
+        //using double data type for decimal numbers
+        System.out.println("Enter the deposit amount: ");
+        double principal = scanner.nextdouble();
+
+
 
     }
 }
